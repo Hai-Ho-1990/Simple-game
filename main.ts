0471f2b626fecf0e6c5fd2f2bac2cbab766e5b34
Hai-Ho-1990 <juno.hu2090@gmail.com>
2024-08-23 12:46:38 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,51 @@
+scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
+    // Initialize the Bat sprite before setting its velocity
+    Bat = sprites.create(img`
+        . . . . . . . . . . b 5 b . . . 
+        . . . . . . . . . b 5 b . . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . . . . b b 5 d 1 f 5 5 d f . . 
+        . . . . b 5 5 1 f f 5 d 4 c . . 
+        . . . . b 5 5 d f b d d 4 4 . . 
+        . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+        b d d d b b d 5 5 4 4 4 4 4 b . 
+        b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+        c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+        c b d c d 5 5 b 5 5 5 5 5 5 b . 
+        . c d d c c b d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    Bat.setVelocity(19, 65)
+})
+let location: tiles.Location = null
+let Bat: Sprite = null
+tiles.setCurrentTilemap(tilemap`level2`)
+let Duck = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+Duck.setPosition(10, 6)
+controller.moveSprite(Duck)
+scene.cameraFollowSprite(Duck)
+// This function will update the tile color as the Duck sprite moves
+game.onUpdate(function () {
+    location = tiles.getTileLocation(Math.floor(Duck.x / 16), Math.floor(Duck.y / 16))
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
